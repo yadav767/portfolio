014ae893d0f75efb9199c71ec12ed71a7c9b76d0
yadav767 <yyadavaayush767@gmail.com>
2026-02-15 02:12:17 +0530

    add public folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 const express =require("express");
+const path=require("path")
 const cors=require("cors")
 const portfolioRouter=require("./routes/portfolio.route")
 const app=express();
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"../Public")))
 
 app.use("/api/portfolio",portfolioRouter)
 
-
+app.get("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../Public/index.html"))
+})
 
 module.exports=app
